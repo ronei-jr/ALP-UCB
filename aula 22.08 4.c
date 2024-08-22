4f9f228230f7a17d18664ad77f09fb56202e2b1e
Ronei Junior <166155096+ronei-jr@users.noreply.github.com>
2024-08-22 11:10:05 -0300

    Add files via upload

---
+++ aula 22.08 4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int compromisso, cidade, dinheiro, chovendo, perto;
+	
+	printf("0 - nao\n1 - sim \n\n");
+	printf("Tenho compromisso no dia? ");
+	scanf("%d", &compromisso);
+	printf("Vou estar na cidade? ");
+	scanf("%d", &cidade);
+	printf("Tenho dinheiro? ");
+	scanf("%d", &dinheiro);
+	printf("E perto? ");
+	scanf("%d", &perto);
+	printf("Esta chovendo? ");
+	scanf("%d", &chovendo);
+	
+	if((compromisso == 0 && cidade == 1 && ((dinheiro == 1 && chovendo == 0) || (dinheiro == 0 && perto == 1)
+	))) {
+	printf
+}
